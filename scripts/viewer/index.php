74238e649d12d4c3e85ac50060faca1b57f18ac9
Dmitri Popov <dpopov@suse.de>
2019-12-23 15:19:57 +0100

    Add file viewer

---
+++ scripts/viewer/index.php
@@ -0,0 +1,45 @@
+<?php 
+define('PROJECT_ROOT', getcwd());
+?> 
+
+<!DOCTYPE html>
+<html> 
+    <head lang="en">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Little Backup Box</title>
+	<!-- Include stylesheet -->
+	<link href="viewer/css/styles.css" rel="stylesheet"/> 
+	<!-- FancyBox --> 
+	<link rel="stylesheet" href="viewer/js/jquery.fancybox.min.css" />
+	<script src="viewer/js/jquery.fancybox.min.js"></script>
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+	</head> 
+	<body>
+			<div class="filemanager">
+			<div class="search">
+				<input type="search" placeholder="Find a file" />
+			</div>
+			<div class="breadcrumbs"></div>
+			<a class="button folderName" id="backButton" href=""><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</a>
+			<a class="button" href="./"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+			<ul class="data"></ul>
+			
+			<!-- Include script files -->
+			<script src="viewer/js/jquery.min.js"></script>
+			<script src="viewer/js/script.js"></script>
+			<script src="viewer/js/jquery.fancybox.min.js"></script>
+			<script>
+			 $('.fancybox-media').fancybox({
+			     type: 'iframe',
+			     width: 800,
+			     height: 580,
+			     fitToView: false,
+			     iframe : {
+				 preload : false
+			     }
+			 }); 
+			</script>
+	</body>
+</html>
---
+++ scripts/viewer/scan.php
@@ -0,0 +1,67 @@
+<?php
+
+$dir = "storage";
+
+// Run the recursive function 
+
+$response = scan($dir);
+
+
+// This function scans the files folder recursively, and builds a large array
+
+function scan($dir){
+
+	$files = array();
+
+	// Is there actually such a folder/file?
+
+	if(file_exists($dir)){
+	
+		foreach(scandir($dir) as $f) {
+		
+			if(!$f || $f[0] == '.') {
+				continue; // Ignore hidden files
+			}
+
+			if(is_dir($dir . '/' . $f)) {
+
+				// The path is a folder
+
+				$files[] = array(
+					"name" => $f,
+					"type" => "folder",
+					"path" => $dir . '/' . $f,
+					"items" => scan($dir . '/' . $f) // Recursively get the contents of the folder
+				);
+			}
+			
+			else if (is_file($dir . '/' . $f)) {
+
+				// It is a file
+
+				$files[] = array(
+					"name" => $f,
+					"type" => "file",
+					"path" => $dir . '/' . $f,
+					"size" => filesize($dir . '/' . $f) // Gets the size of this file
+				);
+			}
+		}
+	
+	}
+
+	return $files;
+}
+
+
+
+// Output the directory listing as JSON
+
+header('Content-type: application/json');
+
+echo json_encode(array(
+	"name" => basename($dir),
+	"type" => "folder",
+	"path" => $dir,
+	"items" => $response
+));
