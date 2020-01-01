3b3aa19dfc96fc3b2784f14cf8c4dbbc485b5eae
Dmitri Popov <dmpop@linux.com>
2020-01-01 21:03:30 +0100

    Icons instead of images

--- scripts/viewer.php
+++ scripts/viewer.php
@@ -7,20 +7,30 @@
         <title>Little Backup Box</title>
         <link rel="shortcut icon" href="favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+	 #content {
+	     margin: 0px auto;
+	     text-align: center;
+	 }
+	</style>
     </head>
-
+    
     <body>
-	<center><a href="/"><div style="margin-bottom: 1.9em;"><img src="logo.svg" height="51px" alt="Little Backup Box"></a></div></center>
-	<?php
-	$ext = Array ( 'jpeg', 'jpg' );
-	$rdi = new RecursiveDirectoryIterator("storage/");
-	$it = new RecursiveIteratorIterator($rdi);
-	foreach($it as $item)
-	if (in_array(strtolower(array_pop(explode('.', $item))), $ext))
-	    echo '<a href="'.$item.'"><img src="'.$item.'" width=100px /></a>';
-	?>
-	<p>
-	    <a href="https://gumroad.com/l/linux-photography"><img src="svg/life-ring.svg" height="35px" alt="Linux Photography book"></a>
-	</p>
+	<div id="content">
+	    <a href="/"><div style="margin-bottom: 1.9em;"><img src="logo.svg" height="51px" alt="Little Backup Box"></a></div>
+	    <div style="display: inline-block; text-align: left;">
+		<?php
+		$ext = Array ( 'jpeg', 'jpg' );
+		$rdi = new RecursiveDirectoryIterator("storage/");
+		$it = new RecursiveIteratorIterator($rdi);
+		foreach($it as $item)
+		if (in_array(strtolower(array_pop(explode('.', $item))), $ext))
+		    echo '<a href="'.$item.'"><img style="vertical-align:middle;" src="svg/image1.svg"></a> <div style="vertical-align:middle; display:inline;">'.basename($item).'</div><br />';
+		?>
+	    </div>
+	    <p>
+		<a href="https://gumroad.com/l/linux-photography"><img src="svg/life-ring.svg" height="35px" alt="Linux Photography book"></a>
+	    </p>
+	</div>
     </body>
 </html>
