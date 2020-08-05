adbbf2e0b1b61e47ef5dbd52a5ee5d5d519b89ad
Dmitri Popov <dmpop@linux.com>
2020-08-05 21:49:33 +0200

    Improve System Info page (gauges)

--- scripts/sysinfo.php
+++ scripts/sysinfo.php
@@ -10,6 +10,8 @@
 	<link rel="stylesheet" href="css/uikit.min.css" />
 	<script src="js/uikit.min.js"></script>
 	<script src="js/uikit-icons.min.js"></script>
+	<script src="js/justgage.js"></script>
+	<script src="js/raphael.min.js"></script>
 </head>
 
 <body>
@@ -20,39 +22,72 @@
 	$i18n->init();
 	?>
 	<div class="uk-container uk-margin-top">
-		<h1><?php echo L::sysinfo; ?></h1>
-		<hr>
-		<h3>Devices</h3>
-		<div style="display: inline-block; text-align: left;">
+		<div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+			<h1 class="uk-card-title uk-heading-line uk-text-center"><?php echo L::sysinfo; ?></h1>
+			<?php
+			$temp = shell_exec('cat /sys/class/thermal/thermal_zone*/temp');
+			$temp = round($temp / 1000, 1);
+			$cpuusage = 100 - shell_exec("vmstat | tail -1 | awk '{print $15}'");
+			$mem = shell_exec("free | grep Mem | awk '{print $3/$2 * 100.0}'");
+			$mem = round($mem, 1);
+			if (isset($temp) && is_numeric($temp)) { ?>
+				<div id="tempgauge"></div>
+				<script>
+					var t = new JustGage({
+						id: "tempgauge",
+						value: <?php echo $temp; ?>,
+						min: 0,
+						max: 100,
+						title: "<?php echo L::temp; ?>",
+						label: "°C"
+					});
+				</script>
+			<?php } ?>
+			<?php if (isset($cpuusage) && is_numeric($cpuusage)) { ?>
+				<div id="cpugauge"></div>
+				<script>
+					var u = new JustGage({
+						id: "cpugauge",
+						value: <?php echo $cpuusage; ?>,
+						min: 0,
+						max: 100,
+						title: "<?php echo L::cpuload; ?>",
+						label: "%"
+					});
+				</script>
+			<?php } ?>
+			<?php if (isset($mem) && is_numeric($mem)) { ?>
+				<div id="memgauge"></div>
+				<script>
+					var u = new JustGage({
+						id: "memgauge",
+						value: <?php echo $mem; ?>,
+						min: 0,
+						max: 100,
+						title: "<?php echo L::memory; ?>",
+						label: "%"
+					});
+				</script>
+			<?php } ?>
+		</div>
+		<div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+			<h3 class="uk-card-title uk-heading-line uk-text-center"><?php echo L::devices; ?></h3>
 			<?php
 			echo '<pre>';
 			passthru("lsblk");
 			echo '</pre>';
 			?>
 		</div>
-		<hr>
-		<h3><?php echo L::diskspace; ?></h3>
-		<p></p>
-		<div style="display: inline-block; text-align: left; margin-bottom: 1.5em;">
+		<div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+			<h3 class="uk-card-title uk-heading-line uk-text-center"><?php echo L::diskspace; ?></h3>
 			<?php
 			echo '<pre>';
 			passthru("df -H");
 			echo '</pre>';
 			?>
 		</div>
-		<hr>
-		<h3><?php echo L::memory; ?></h3>
-		<div style="display: inline-block; text-align: left; margin-bottom: 2em;">
-			<?php
-			echo '<pre>';
-			passthru("free -m");
-			echo '</pre>';
-			?>
-		</div>
-		<form>
-			<button class="uk-button uk-button-primary uk-margin-top" onClick="history.go(0)" role="button"><?php echo L::refresh_b; ?></button>
-			<a class="uk-button uk-button-default uk-margin-top" href="index.php"><?php echo L::back_b; ?></a>
-		</form>
+		<button class="uk-button uk-button-primary uk-margin-top" onClick="history.go(0)" role="button"><?php echo L::refresh_b; ?></button>
+		<a class="uk-button uk-button-default uk-margin-top" href="index.php"><?php echo L::back_b; ?></a>
 	</div>
 </body>
 
