1484f86bf424d54bb19ac96dae38589de539a2fb
Dmitri Popov <dmpop@linux.com>
2020-08-30 10:57:10 +0200

    Minor tweaks

--- scripts/ip.php
+++ scripts/ip.php
@@ -1,9 +1,10 @@
 <html lang="en">
+	
 <!-- Author: Dmitri Popov, dmpop@linux.com
          License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 
 <head>
-	<title>Little Backup Box IP Address</title>
+	<title>Little Backup Box</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.6/dist/css/uikit.min.css" />
@@ -14,17 +15,19 @@
 <body>
 	<div class="uk-container uk-margin-small-top">
 		<div class="uk-card uk-card-primary uk-card-body uk-text-center">
-			<h1 class="uk-card-title uk-heading-line uk-text-center"><span>IP address</span></h1>
-			<?php
-			if (isset($_GET["ip"])) {
-				echo $_GET['ip'];
-				$f = fopen("ip", "w");
-				fwrite($f, $_GET['ip']);
-				fclose($f);
-			} else {
-				echo file_get_contents('ip', true);
-			}
-			?>
+			<h1 class="uk-heading-line uk-text-center"><span>IP address</span></h1>
+			<h1>
+				<?php
+				if (isset($_GET["ip"])) {
+					echo $_GET['ip'];
+					$f = fopen("ip", "w");
+					fwrite($f, $_GET['ip']);
+					fclose($f);
+				} else {
+					echo file_get_contents('ip', true);
+				}
+				?>
+			</h1>
 		</div>
 	</div>
 </body>
