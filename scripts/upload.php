95c6d483ba249a97c9a2bac07d06a485e2807f57
Dmitri Popov <dmpop@linux.com>
2020-10-02 10:13:48 +0200

    Move  up

--- scripts/upload.php
+++ scripts/upload.php
@@ -1,3 +1,8 @@
+<?php
+// Upload directory
+$upload_dir = getenv("HOME") . "/UPLOAD";
+?>
+
 <html lang="en">
 <!-- Author: Dmitri Popov, dmpop@linux.com
          License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
@@ -13,34 +18,33 @@
 </head>
 
 <body>
-<div class="uk-container uk-margin-small-top">
+	<div class="uk-container uk-margin-small-top">
 		<div class="uk-card uk-card-primary uk-card-body uk-text-center">
-		<?php
-		// include i18n class and initialize it
-		require_once 'i18n.class.php';
-		$i18n = new i18n('lang/{LANGUAGE}.ini', 'cache/', 'en');
-		$i18n->init();
-		$upload_dir = getenv("HOME") . "/UPLOAD";
-		if (isset($_POST['submit'])) {
-			// count total files
-			$countfiles = count($_FILES['file']['name']);
-			// looping all files
-			for ($i = 0; $i < $countfiles; $i++) {
-				$filename = $_FILES['file']['name'][$i];
-				if (!file_exists($upload_dir)) {
-					mkdir($upload_dir, 0777, true);
+			<?php
+			// include i18n class and initialize it
+			require_once 'i18n.class.php';
+			$i18n = new i18n('lang/{LANGUAGE}.ini', 'cache/', 'en');
+			$i18n->init();
+			if (isset($_POST['submit'])) {
+				// count total files
+				$countfiles = count($_FILES['file']['name']);
+				// looping all files
+				for ($i = 0; $i < $countfiles; $i++) {
+					$filename = $_FILES['file']['name'][$i];
+					if (!file_exists($upload_dir)) {
+						mkdir($upload_dir, 0777, true);
+					}
+					// upload file
+					move_uploaded_file($_FILES['file']['tmp_name'][$i], $upload_dir . DIRECTORY_SEPARATOR . $filename);
 				}
-				// upload file
-				move_uploaded_file($_FILES['file']['tmp_name'][$i], $upload_dir . DIRECTORY_SEPARATOR . $filename);
 			}
-		}
-		?>
-		<h1 class="uk-heading-line uk-text-center"><span><?php echo L::upload; ?></span></h1>
-		<form method='post' action='' enctype='multipart/form-data'>
-			<input class="uk-input" type="file" name="file[]" id="file" multiple>
-			<button class="uk-button uk-button-primary uk-margin-top" type='submit' role='button' name='submit'><?php echo L::upload; ?></button>
-			<a class="uk-button uk-button-default uk-margin-top" href="index.php"><?php echo L::back_b; ?></a>
-		</form>
+			?>
+			<h1 class="uk-heading-line uk-text-center"><span><?php echo L::upload; ?></span></h1>
+			<form method='post' action='' enctype='multipart/form-data'>
+				<input class="uk-input" type="file" name="file[]" id="file" multiple>
+				<button class="uk-button uk-button-primary uk-margin-top" type='submit' role='button' name='submit'><?php echo L::upload; ?></button>
+				<a class="uk-button uk-button-default uk-margin-top" href="index.php"><?php echo L::back_b; ?></a>
+			</form>
 		</div>
 	</div>
 </body>
