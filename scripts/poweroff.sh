9482ded88b171d05e6720666d37c5014e76c6a7a
Dmitri Popov <dmpop@linux.com>
2021-07-25 16:45:40 +0200

    Remove rsync log on poweroff

--- scripts/poweroff.sh
+++ scripts/poweroff.sh
@@ -26,6 +26,7 @@ source "$CONFIG"
 FILE_OLED_OLD="/root/oled_old.txt"
 FILE_LOG="tmp/little-backup-box.log"
 FSCK_LOG="tmp/fsck.log"
+RSYNC_LOG="/root/little-backup-box.log"
 # Load LCD library
 . "${WORKING_DIR}/lib-lcd.sh"
 
@@ -51,6 +52,7 @@ if [ "$POWER_OFF" = "true" ] || [ "$MODE" = "force" ]; then
     echo "" > "${FILE_OLED_OLD}"
     rm "${FILE_LOG}"
     rm "${FSCK_LOG}"
+    rm "${RSYNC_LOG}"
 
     poweroff
 else
