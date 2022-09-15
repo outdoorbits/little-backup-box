5511dfb61df52ac6c636cedbf42a8ac8d24265b3
Stefan Saam <stefan@saams.de>
2022-09-15 21:13:21 +0200

    use full resolution of 64-lines display

--- scripts/oled.py
+++ scripts/oled.py
@@ -38,7 +38,7 @@ i2c = busio.I2C(SCL, SDA)
 # Create the SSD1306 OLED class.
 # The first two parameters are the pixel width and pixel height.  Change these
 # to the right size for your display!
-disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
 
 # Clear display.
 disp.fill(0)
@@ -55,15 +55,14 @@ draw = ImageDraw.Draw(image)
 
 # Draw some shapes.
 # First define some constants to allow easy resizing of shapes.
-top = -2
+top = 0
 
 # define font
 font = ImageFont.load_default()
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
-
+font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 12)
 # Move left to right keeping track of the current x position for drawing shapes.
 x			= 0
 line_height	= int((height - top)/4)
