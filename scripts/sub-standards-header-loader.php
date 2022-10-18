05ebc1d4607ebbdf91ca6ef52bccc56790c51620
Stefan Saam <stefan@saams.de>
2022-10-18 13:56:55 +0200

    bugfix

--- scripts/sub-standards-header-loader.php
+++ scripts/sub-standards-header-loader.php
@@ -2,7 +2,7 @@
          License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 
 <title>Little Backup Box</title>
-<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="shortcut icon" href="favicon.png" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
