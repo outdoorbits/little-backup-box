3f11e0f4eaf3a55fc8352614fb8b92cb51b36930
Stefan Saam <stefan@saams.de>
2023-03-16 22:51:13 +0100

    minor fixes

--- scripts/cmd-runner.php
+++ scripts/cmd-runner.php
@@ -22,13 +22,13 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 		<?php
 			extract($_GET);
 
-			if (!isset($MAIL_RESULT)) {$MAIL_RESULT=False;}
+			$MAIL_RESULT = isset($MAIL_RESULT);
 
 			if ((strpos($_SERVER["HTTP_REFERER"],"/cmd.php") !== False) and isset($CMD)) {
 
 				switch($CMD) {
 					case 'update':
-						$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_update_start1 . "' '" . L::box_cmd_update_start2 . "'";
+						$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_update_start1 . "' ':" . L::box_cmd_update_start2 . "'";
 						$COMMAND_LINE	.= ";cd ~pi; curl -sSL https://raw.githubusercontent.com/outdoorbits/little-backup-box/main/install-little-backup-box.sh | sudo -u pi bash";
 						break;
 
@@ -37,7 +37,7 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 							if ($PARAM2 == "FAT32") {
 								$MAIN_COMMAND	= "mkfs.vfat -v -I -F32 /dev/$PARAM1";
 
-								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_start1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_start2 . "'";
+								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_start1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_start2 . "'";
 								$COMMAND_LINE	.= ";sudo umount /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo 'sudo $MAIN_COMMAND'";
 								$COMMAND_LINE	.= ";echo ''";
@@ -50,13 +50,13 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 								$COMMAND_LINE	.= ";sudo fsck /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo ''";
 								$COMMAND_LINE	.= ";echo 'FINISHED.'";
-								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_stop1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_stop2 . "'";
+								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_stop1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_stop2 . "'";
 							}
 
 							if ($PARAM2 == "exFAT") {
 								$MAIN_COMMAND	= "mkfs.exfat /dev/$PARAM1";
 
-								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_start1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_start2 . "'";
+								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_start1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_start2 . "'";
 								$COMMAND_LINE	.= ";sudo umount /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo 'sudo $MAIN_COMMAND'";
 								$COMMAND_LINE	.= ";echo ''";
@@ -69,13 +69,13 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 								$COMMAND_LINE	.= ";sudo fsck /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo ''";
 								$COMMAND_LINE	.= ";echo 'FINISHED.'";
-								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_stop1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_stop2 . "'";
+								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_stop1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_stop2 . "'";
 							}
 
 							if ($PARAM2 == "NTFS (compression enabled)") {
 								$MAIN_COMMAND	= "mkfs.ntfs --enable-compression --force --verbose /dev/$PARAM1";
 
-								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_start1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_start2 . "'";
+								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_start1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_start2 . "'";
 								$COMMAND_LINE	.= ";sudo umount /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo 'sudo $MAIN_COMMAND'";
 								$COMMAND_LINE	.= ";echo ''";
@@ -88,13 +88,13 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 								$COMMAND_LINE	.= ";sudo fsck /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo ''";
 								$COMMAND_LINE	.= ";echo 'FINISHED.'";
-								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_stop1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_stop2 . "'";
+								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_stop1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_stop2 . "'";
 							}
 
 							if ($PARAM2 == "NTFS (no compression)") {
 								$MAIN_COMMAND	= "mkfs.ntfs --force --verbose /dev/$PARAM1";
 
-								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_start1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_start2 . "'";
+								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_start1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_start2 . "'";
 								$COMMAND_LINE	.= ";sudo umount /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo 'sudo $MAIN_COMMAND'";
 								$COMMAND_LINE	.= ";echo ''";
@@ -107,13 +107,13 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 								$COMMAND_LINE	.= ";sudo fsck /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo ''";
 								$COMMAND_LINE	.= ";echo 'FINISHED.'";
-								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_stop1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_stop2 . "'";
+								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_stop1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_stop2 . "'";
 							}
 
 							if ($PARAM2 == "Ext4") {
 								$MAIN_COMMAND	= "mkfs.ext4 -v -F /dev/$PARAM1";
 
-								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_start1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_start2 . "'";
+								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_start1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_start2 . "'";
 								$COMMAND_LINE	.= ";sudo umount /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo 'sudo $MAIN_COMMAND'";
 								$COMMAND_LINE	.= ";echo ''";
@@ -126,13 +126,13 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 								$COMMAND_LINE	.= ";sudo fsck /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo ''";
 								$COMMAND_LINE	.= ";echo 'FINISHED.'";
-								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_stop1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_stop2 . "'";
+								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_stop1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_stop2 . "'";
 							}
 
 							if ($PARAM2 == "Ext3") {
 								$MAIN_COMMAND	= "mkfs.ext3 -v -F /dev/$PARAM1";
 
-								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_start1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_start2 . "'";
+								$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_start1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_start2 . "'";
 								$COMMAND_LINE	.= ";sudo umount /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo 'sudo $MAIN_COMMAND'";
 								$COMMAND_LINE	.= ";echo ''";
@@ -145,7 +145,7 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 								$COMMAND_LINE	.= ";sudo fsck /dev/$PARAM1";
 								$COMMAND_LINE	.= ";echo ''";
 								$COMMAND_LINE	.= ";echo 'FINISHED.'";
-								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_format_stop1 . "' '$PARAM1: $PARAM2' '" . L::box_cmd_format_stop2 . "'";
+								$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_format_stop1 . "' ':$PARAM1: $PARAM2' ':" . L::box_cmd_format_stop2 . "'";
 							}
 						} else {
 							$COMMAND_LINE	= "";
@@ -157,27 +157,27 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 							case 'f3probe_non_destructive':
 									$MAIN_COMMAND	= "f3probe --time-ops /dev/$PARAM1";
 
-									$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_f3_probe_start1 . "' '$PARAM1: " . L::box_cmd_f3_probe_non_destructive . "' '" . L::box_cmd_f3_probe_start2 . "'";
+									$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_f3_probe_start1 . "' ':$PARAM1: " . L::box_cmd_f3_probe_non_destructive . "' ':" . L::box_cmd_f3_probe_start2 . "'";
 									$COMMAND_LINE	.= ";sudo umount /dev/$PARAM1";
 									$COMMAND_LINE	.= ";echo 'sudo $MAIN_COMMAND'";
 									$COMMAND_LINE	.= ";echo ''";
 									$COMMAND_LINE	.= ";sudo $MAIN_COMMAND";
 									$COMMAND_LINE	.= ";echo ''";
 									$COMMAND_LINE	.= ";echo 'FINISHED.'";
-									$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_f3_probe_stop1 . "' '$PARAM1: " . L::box_cmd_f3_probe_non_destructive . "' '" . L::box_cmd_f3_probe_stop2 . "'";
+									$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_f3_probe_stop1 . "' ':$PARAM1: " . L::box_cmd_f3_probe_non_destructive . "' ':" . L::box_cmd_f3_probe_stop2 . "'";
 								break;
 
 							case 'f3probe_destructive':
 									$MAIN_COMMAND	= "f3probe --destructive --time-ops /dev/$PARAM1";
 
-									$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_f3_probe_start1 . "' '$PARAM1: " . L::box_cmd_f3_probe_destructive . "' '" . L::box_cmd_f3_probe_start2 . "'";
+									$COMMAND_LINE	= "sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_f3_probe_start1 . "' ':$PARAM1: " . L::box_cmd_f3_probe_destructive . "' ':" . L::box_cmd_f3_probe_start2 . "'";
 									$COMMAND_LINE	.= ";sudo umount /dev/$PARAM1";
 									$COMMAND_LINE	.= ";echo 'sudo $MAIN_COMMAND'";
 									$COMMAND_LINE	.= ";echo ''";
 									$COMMAND_LINE	.= ";sudo $MAIN_COMMAND";
 									$COMMAND_LINE	.= ";echo ''";
 									$COMMAND_LINE	.= ";echo 'FINISHED.'";
-									$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh '" . L::box_cmd_f3_probe_stop1 . "' '$PARAM1: " . L::box_cmd_f3_probe_destructive . "' '" . L::box_cmd_f3_probe_stop2 . "'";
+									$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh ':" . L::box_cmd_f3_probe_stop1 . "' ':$PARAM1: " . L::box_cmd_f3_probe_destructive . "' ':" . L::box_cmd_f3_probe_stop2 . "'";
 								break;
 
 							default:
@@ -193,7 +193,7 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 									$COMMAND_LINE	.= ";sudo $MAIN_COMMAND";
 									$COMMAND_LINE	.= ";echo ''";
 									$COMMAND_LINE	.= ";echo 'FINISHED.'";
-									$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh '" . L::config_comitup_section . "' '" . L::cmd_reset . "'";
+									$COMMAND_LINE	.= ";sudo $WORKING_DIR/lib-display-helper.sh ':" . L::config_comitup_section . "' ':" . L::cmd_reset . "'";
 								break;
 					default:
 						$COMMAND_LINE	= "";
@@ -230,12 +230,13 @@ License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
 					echo '</pre>';
 
 					unlink($constants["const_CMD_RUNNER_LOCKFILE"]);
+
+					if ($MAIL_RESULT) {
+						shell_exec('sudo ' . $WORKING_DIR . '/lib-mail-helper.sh "' . $CMD . ' ' . $PARAM1 . ' ' . $PARAM2 . '" "' . $RESULT . '" "' . str_replace('\n','<br>\n',$RESULT) . '"');
+					}
 				}
 			} else {
 				echo "NOT AUTHORISED";
-			}
-			if ($MAIL_RESULT) {
-				shell_exec('sudo ' . $WORKING_DIR . '/lib-mail-helper.sh "' . $CMD . ' ' . $PARAM1 . ' ' . $PARAM2 . '" "' . $RESULT . '" "' . str_replace('\n','<br>\n',$RESULT) . '"');
 			}
 		?>
 
