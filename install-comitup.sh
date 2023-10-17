bcf1277be420af3f5bec225c2b1cba0a7d10a799
outdoorbits <github@outdoorbits.org>
2023-10-17 14:13:29 +0200

    bugfix comitup installer

--- install-comitup.sh
+++ install-comitup.sh
@@ -26,30 +26,11 @@ fi
 
 echo "Installing comitup..."
 
-# install dependencies
-sudo DEBIAN_FRONTEND=noninteractive \
-		apt \
-		-o "Dpkg::Options::=--force-confold" \
-		-o "Dpkg::Options::=--force-confdef" \
-		install -y -q --allow-downgrades --allow-remove-essential --allow-change-held-packages \
-		dnsmasq systemd-resolved dhcpd dhcpcd wpasupplicant
-
-# Installing comitup*: install comitup sources
-wget https://davesteele.github.io/comitup/latest/davesteele-comitup-apt-source_latest.deb
-sudo dpkg -i --force-all davesteele-comitup-apt-source_latest.deb
-sudo rm davesteele-comitup-apt-source_latest.deb
-sudo apt update
-
 # Enable and start NetworkManager
 sudo systemctl enable NetworkManager.service
 
 # Installing comitup*: install comitup
-sudo DEBIAN_FRONTEND=noninteractive \
-		apt \
-		-o "Dpkg::Options::=--force-confold" \
-		-o "Dpkg::Options::=--force-confdef" \
-		install -y -q --allow-downgrades --allow-remove-essential --allow-change-held-packages \
-		comitup comitup-watch
+sudo apt-get install comitup
 
 # Installing comitup*: Allow NetworkManager to manage the wifi interfaces by removing references to them from /etc/network/interfaces
 sudo rm /etc/network/interfaces
@@ -61,13 +42,6 @@ fi
 if [ -f "/etc/wpa_supplicant/wpa_supplicant.conf" ]; then
 	sudo mv /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.old
 fi
-
-# Installing comitup*: The systemd.resolved service should be disabled and masked to avoid contention for providing DNS service. EDIT - there is a reported alternate method to keep the service from interfering.
-sudo systemctl unmask dnsmasq.service
-sudo systemctl unmask systemd-resolved.service
-sudo systemctl unmask dhcpd.service
-sudo systemctl unmask dhcpcd.service
-sudo systemctl unmask wpa-supplicant.service
 
 # create config
 sudo python3 "${INSTALLER_DIR}/scripts/lib_comitup.py"
