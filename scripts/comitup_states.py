be8999f3d19c8335e1ca75e1adc93098a0f63c63
outdoorbits <github@outdoorbits.org>
2023-11-08 17:52:50 +0100

    minor tweaks

--- scripts/comitup_states.py
+++ scripts/comitup_states.py
@@ -26,9 +26,10 @@ class comitup_states(object):
 	def __init__(self):
 		self.__display	= lib_display.display()
 
-	def new_state(self,state):
-		# display new state
-		self.__display.message([':Comitup', f':{state}'])
+	def new_status(self,status):
+		# display new status
+		if status:
+			self.__display.message([':Comitup:', f':{status}'])
 
 		# setup apache ports
 		ApachePortsConf	= '/etc/apache2/ports.conf'
@@ -45,14 +46,15 @@ class comitup_states(object):
 				f.write(f'Listen {Port}\n')
 
 			if len(sys.argv) > 1:
-				if state == 'CONNECTED':
+				if status == 'CONNECTED':
 					f.write(f'Listen 80\n')
 
 		subprocess.run('service apache2 restart || service apache2 start', shell=True)
 
 if __name__ == "__main__":
-	state	= ''
-	if len(sys.argv) > 1:
-		state = sys.argv[1]
+	try:
+		status	= sys.argv[1]
+	except:
+		status	= ''
 
-	comitup_states().new_state(state)
+	comitup_states().new_status(status)
